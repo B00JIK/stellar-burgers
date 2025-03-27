d7921b7fbceb8191c7fe380c5d07f14ef27d24e5
Kate Timofeeva <timofeeva.katerina2016@yandex.ru>
2025-03-27 14:50:45 +0300

    first try

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,21 +1,22 @@
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
 import { TIngredient } from '@utils-types';
+import { useDispatch, useSelector } from '../../services/store';
+import { useParams } from 'react-router-dom';
+import { getOrderByNumber } from '../../services/slices/orderSlice';
 
 export const OrderInfo: FC = () => {
   /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+  const orderData = useSelector((store) => store.order.order);
+  const dispatch = useDispatch();
+  const params = useParams();
+  const orderNubmer = Number(params.number);
+  const ingredients = useSelector((store) => store.ingredients.ingredients);
 
-  const ingredients: TIngredient[] = [];
+  useEffect(() => {
+    dispatch(getOrderByNumber(orderNubmer));
+  }, [dispatch]);
 
   /* Готовим данные для отображения */
   const orderInfo = useMemo(() => {

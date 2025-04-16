f80ce3f3a0bd2c36e6236ebc369924226bc5f60c
Kate Timofeeva <timofeeva.katerina2016@yandex.ru>
2025-04-16 18:36:37 +0300

    first try

--- src/services/store.test.ts
+++ src/services/store.test.ts
@@ -5,7 +5,7 @@ import store from './store';
 describe('Проверка правильной инициализации rootReducer', () => {
   test('Проверка корректного возвращения начального состояния', () => {
     const previousStore = store.getState();
-    const action = { type: 'ACTION' };
+    const action = { type: 'UNKNOWN_ACTION' };
     const initialState = rootReducer(undefined, action);
 
     expect(initialState).toEqual(previousStore);

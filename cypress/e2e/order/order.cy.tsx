50ff9ab62a43ad4d9d66f936150334faa6aa1429
Kate Timofeeva <timofeeva.katerina2016@yandex.ru>
2025-04-18 12:30:54 +0300

    third try

--- cypress/e2e/order/order.cy.tsx
+++ cypress/e2e/order/order.cy.tsx
@@ -18,7 +18,6 @@ describe('тест оформления заказа', function() {
         cy.get('[data-cy="mains"]').as('mains');
         cy.get('[data-cy="buns"]').as('buns');
         cy.get('[data-cy="order"]').as('order');
-        cy.get('[data-cy="order_number"]').as('order_number');
         cy.get('[data-cy="top_bun"]').as('top_bun');
         cy.get('[data-cy="ingredient"]').as('ingredient');
         cy.get('[data-cy="bottom_bun"]').as('bottom_bun');
@@ -31,7 +30,7 @@ describe('тест оформления заказа', function() {
         cy.get('@mains').contains('Добавить').click();
         cy.get('@buns').contains('Добавить').click();
         cy.get('@order').contains('Оформить заказ').click();
-        cy.get('@order_number').contains('7472').should('exist');
+        cy.get('[data-cy="order_number"]').contains('7472').should('exist');
         cy.get('@top_bun').contains("Краторная булка N-200i").should('not.exist');
         cy.get('@ingredient').contains("Биокотлета из марсианской Магнолии").should('not.exist');
         cy.get('@bottom_bun').contains("Краторная булка N-200i").should('not.exist');
